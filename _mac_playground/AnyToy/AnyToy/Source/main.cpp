f3d6ccfe7f006f4cfea720b3386288ffad9a0d7f
miaoyunlong <miaoyunlong@bytedance.com>
2020-12-31 13:36:09 +0800

    +add base lua lib

---
+++ _mac_playground/AnyToy/AnyToy/Source/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AnyToy
+//
+//  Created by bytedance on 2020/12/23.
+//
+
+#include <iostream>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
